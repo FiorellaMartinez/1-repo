c41b7ccc5c41b5b515b30abaefa2dc67d0288b02
fiorella <fiorella.geminis.10@gmail.com>
2026-04-21 23:08:59 -0500

    agregue texto

---
+++ Hola.java
@@ -0,0 +1,18 @@
+public class hola {
+
+    public static void main(String[] args) {
+        
+        // Imprimir mensaje en consola
+        System.out.println("Hola, mundo!");
+
+        // Declarar variables
+        int numero1 = 5;
+        int numero2 = 3;
+
+        // Realizar suma
+        int resultado = numero1 + numero2;
+
+        // Mostrar resultado
+        System.out.println("La suma es: " + resultado);
+    }
+}
